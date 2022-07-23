4d7e7e76a45a2ba633dfcb4c341b6da9e60e6378
FeiHongBo <122429174@qq.com>
2022-07-23 20:35:36 +0800

    first commit

---
+++ point.h
@@ -0,0 +1,22 @@
+#pragma once
+#include<iostream>
+//点类
+class Point
+{
+private:
+	int m_X;
+	int m_Y;
+public:
+	//设置X
+	void setX(int x);
+
+	//获取x
+	int getX();
+
+	//设置Y
+	void setY(int y);
+
+	//获取x
+	int getY();
+
+};
